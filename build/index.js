f7ce2d1f74d75373861b1d765fafe0bbc4463f94
Anton <anton@adc.sh>
2019-04-20 14:57:13 +0300

    b

--- build/index.js
+++ build/index.js
@@ -1,29 +1,133 @@
-const { debuglog } = require('util');
-
-const LOG = debuglog('@goa/vary')
+/*!
+ * vary
+ * Copyright(c) 2014-2017 Douglas Christopher Wilson
+ * MIT Licensed
+ */
 
 /**
- * [fork] Manipulate the HTTP Vary header
- * @param {Config} [config] Options for the program.
- * @param {boolean} [config.shouldRun=true] A boolean option. Default `true`.
- * @param {string} config.text A text to return.
+ * RegExp to match field-name in RFC 7230 sec 3.2
+ *
+ * field-name    = token
+ * token         = 1*tchar
+ * tchar         = "!" / "#" / "$" / "%" / "&" / "'" / "*"
+ *               / "+" / "-" / "." / "^" / "_" / "`" / "|" / "~"
+ *               / DIGIT / ALPHA
+ *               ; any VCHAR, except delimiters
  */
-               async function vary(config = {}) {
-  const {
-    shouldRun = true,
-    text,
-  } = config
-  if (!shouldRun) return
-  LOG('@goa/vary called with %s', text)
-  return text
+
+const FIELD_NAME_REGEXP = /^[!#$%&'*+\-.^_`|~0-9A-Za-z]+$/
+
+/**
+ * Append a field to a vary header.
+ * @param {string} header
+ * @param {string|!Array<string>} field
+ */
+       function append(header, field) {
+  if (typeof header != 'string')
+    throw new TypeError('header argument is required')
+
+  if (!field)
+    throw new TypeError('field argument is required')
+
+  // get fields array
+  const fields = !Array.isArray(field) ? parse(`${field}`) : field
+
+  // assert on invalid field names
+  for (let j = 0; j < fields.length; j++) {
+    if (!FIELD_NAME_REGEXP.test(fields[j])) {
+      throw new TypeError('field argument contains an invalid header name')
+    }
+  }
+
+  // existing, unspecified vary
+  if (header == '*') return header
+
+  // enumerate current values
+  var val = header
+  const vals = parse(header.toLowerCase())
+
+  // unspecified vary
+  if (fields.includes('*') || vals.includes('*'))
+    return '*'
+
+  for (let i = 0; i < fields.length; i++) {
+    const fld = fields[i].toLowerCase()
+
+    // append value (case-preserving)
+    if (!vals.includes(fld)) {
+      vals.push(fld)
+      val = val
+        ? val + ', ' + fields[i]
+        : fields[i]
+    }
+  }
+
+  return val
 }
 
-/* documentary types/index.xml */
 /**
- * @typedef {Object} Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
+ * Parse a vary header into an array.
+ * @param {string} header
+ */
+function parse(header) {
+  var end = 0
+  var list = []
+  var start = 0
+
+  // gather tokens
+  for (var i = 0, len = header.length; i < len; i++) {
+    switch (header.charCodeAt(i)) {
+    case 0x20: /*   */
+      if (start == end) {
+        start = end = i + 1
+      }
+      break
+    case 0x2c: /* , */
+      list.push(header.substring(start, end))
+      start = end = i + 1
+      break
+    default:
+      end = i + 1
+      break
+    }
+  }
+
+  // final token
+  list.push(header.substring(start, end))
+
+  return list
+}
+
+/**
+ * Mark that a request is varied on a header field.
+ *
+ * @param {http.ServerResponse} res
+ * @param {string|!Array<string>} field
+ */
+               function vary(res, field) {
+  if (!res || !res.getHeader || !res.setHeader) {
+    // quack quack
+    throw new TypeError('res argument is required')
+  }
+
+  // get existing header
+  let val = res.getHeader('Vary') || ''
+  const header = Array.isArray(val)
+    ? val.join(', ')
+    : `${val}`
+
+  // set new header
+  if ((val = append(header, field))) {
+    res.setHeader('Vary', val)
+  }
+}
+
+/* typal types/index.xml */
+/**
+ * @suppress {nonStandardJsDocs}
+ * @typedef {import('http').ServerResponse} http.ServerResponse
  */
 
 
 module.exports = vary
+module.exports.append = append
